6053e67097b6a2cadb1aaa86c9b046c80f7d0c60
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-27 08:35:47 +0600

    multiple.js

---
+++ multiple.js
@@ -0,0 +1,18 @@
+var salary = 1000;
+if(salary > 100){
+    console.log('okay');
+}else{
+    console.log('not okay');
+}
+
+
+const salaryEarn = 1000;
+const height = 6;
+const car = 1;
+const house = 3;
+
+if((salary > 500 && height > 9) || (car >= 2 && house)){
+    console.log("okay");
+}else{
+    console.log('not okay');
+}
